cc1239383c355cb6af2e46d7051b710ec084b5d6
Jack <108418371+ZolarEclipse@users.noreply.github.com>
2024-08-12 12:08:24 -0500

    added questions

--- index.js
+++ index.js
@@ -5,8 +5,38 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please give a description of your project:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'GPLv3', 'Apache 2.0', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide test instructions:',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    }];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
@@ -15,8 +45,6 @@ function init() {}
 
 // Function call to initialize app
 init();
-w
-
 
 
 
